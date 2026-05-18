dc4472afefd6b174595106e4a48bfe11cccb39b6
wada negassa <promisehispeace@gmail.com>
2026-05-18 11:51:03 +0300

    Update windows generated_plugins.cmake

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,18 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  app_links
+  connectivity_plus
+  file_selector_windows
+  flutter_sound
+  pdfx
+  permission_handler_windows
+  speech_to_text_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
